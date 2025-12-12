a585da1c715b5c7757ba63976a12f6b4bc3cc7e1
Jordon <jordonsoo123@gmail.com>
2025-12-12 14:35:39 +0800

    update medicine model

--- src/main/java/model/Medicine.java
+++ src/main/java/model/Medicine.java
@@ -2,59 +2,42 @@ package main.java.model;
 
 // Removed I/O, Scanner, and controller.Home imports
 
-public class Medicine extends Item
-{
-	
+public class Medicine extends Item {
+
 	private String ForDisease;
 	private int amountDaytake;
-	
-	public Medicine()
-	{
-		super("","",0.0,0);
+
+	public Medicine() {
+		super("", "", 0.0, 0);
 		ForDisease = "";
 		amountDaytake = 0;
-		
+
 	}
-	
-	public Medicine(String ItemCode,String ItemDescription,double ItemPrice,int ItemQuantity, String ForDisease, int amountDaytake)
-	{
-		super(ItemCode,ItemDescription,ItemPrice,ItemQuantity);
+
+	public Medicine(String ItemCode, String ItemDescription, double ItemPrice, int ItemQuantity, String ForDisease,
+			int amountDaytake) {
+		super(ItemCode, ItemDescription, ItemPrice, ItemQuantity);
 		this.ForDisease = ForDisease;
 		this.amountDaytake = amountDaytake;
 	}
 
-///////////////////////////////////////////////////////////////////////////////////////
-//Set method
-	public void setForDisease(String ForDisease)
-	{
+	public void setForDisease(String ForDisease) {
 		this.ForDisease = ForDisease;
 	}
-	
-	public void setamountDaytake(int amountDaytake)
-	{
+
+	public void setamountDaytake(int amountDaytake) {
 		this.amountDaytake = amountDaytake;
 	}
-	
-///////////////////////////////////////////////////////////////////////////////////////
-//Get method
-	public String getForDisease()
-	{
-	return ForDisease;
-	}
-	
-	public int getamountDaytake()
-	{
-	return amountDaytake;
-	}
-	
-///////////////////////////////////////////////////////////////////////////////////////
-// The entire 'public static void deleteMedicine(String deleteCode)' method is REMOVED.
- 
-///////////////////////////////////////////////////////////////////////////////////////
-//toString function
 
-public String toString()
-{
-	return super.toString() + "\n For Disease\t\t: " + ForDisease + "\n Amount day take\t: " + amountDaytake;
-}
+	public String getForDisease() {
+		return ForDisease;
+	}
+
+	public int getamountDaytake() {
+		return amountDaytake;
+	}
+
+	public String toString() {
+		return super.toString() + "\n For Disease\t\t: " + ForDisease + "\n Amount day take\t: " + amountDaytake;
+	}
 }
